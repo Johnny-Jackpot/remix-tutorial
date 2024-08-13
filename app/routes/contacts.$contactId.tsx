8674538987e34e969040ea3c5e9545e27473d73a
Johnny-Jackpot <sanya.nazarenko.up036@gmail.com>
2024-08-14 00:43:50 +0300

    loaded contact page

--- app/routes/contacts.$contactId.tsx
+++ app/routes/contacts.$contactId.tsx
@@ -1,20 +1,22 @@
 import { FunctionComponent } from "react";
-import { ContactRecord } from "~/data";
-import { Form } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import invariant from "tiny-invariant";
+import { ContactRecord, getContact } from "~/data";
 import { ContactName } from "~/components/contact-name";
 
+export const loader = async ({ params }: LoaderFunctionArgs) => {
+  invariant(params.contactId, "Missing contactId param");
+  const contact = await getContact(params.contactId);
+  if (!contact) {
+    throw new Response("Not Found", { status: 404 });
+  }
+  return json({ contact });
+};
+
 export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar:
-      "https://img-cdn.pixlr.com/image-generator/history/65bb506dcb310754719cf81f/ede935de-1138-4f66-8ed7-44bd16efc709/medium.webp",
-    twitter: "therock",
-    notes: "Some notes",
-    favorite: true,
-    id: "3443574534534543",
-    createdAt: new Date().toDateString(),
-  } satisfies ContactRecord;
+  const { contact } = useLoaderData<typeof loader>();
 
   return (
     <div id="contact">
